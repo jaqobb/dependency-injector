75fb9412a36ba0378e137bd8cea39c043d2aa3b3
Jakub Zagórski <jaqobbdev@gmail.com>
2018-09-25 22:22:32 +0200

    Update project to use Java 11. Add license to all files. Automatically build source on compile.

--- src/main/java/co/jaqobb/dependencyinjector/DependencyInjector.java
+++ src/main/java/co/jaqobb/dependencyinjector/DependencyInjector.java
@@ -1,3 +1,26 @@
+/*
+ * This file is a part of dependency-injector, licensed under the MIT License.
+ *
+ * Copyright (c) Jakub Zagórski (jaqobb)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package co.jaqobb.dependencyinjector;
 
 import co.jaqobb.dependencyinjector.dependency.Dependency;
@@ -14,14 +37,14 @@ import java.util.Objects;
 
 public final class DependencyInjector
 {
-    private static final Method ADD_URL_METHOD;
+    private static Method ADD_URL_METHOD;
 
     static
     {
         try
         {
-            ADD_URL_METHOD = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
-            ADD_URL_METHOD.setAccessible(true);
+            DependencyInjector.ADD_URL_METHOD = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            DependencyInjector.ADD_URL_METHOD.setAccessible(true);
         }
         catch (NoSuchMethodException exception)
         {
@@ -38,38 +61,38 @@ public final class DependencyInjector
         Objects.requireNonNull(dependencies, "dependencies");
         for (Dependency dependency : dependencies)
         {
-            injectDependency(dependency, classLoader);
+            DependencyInjector.injectDependency(dependency, classLoader);
         }
     }
 
     public static void injectDependency(String shorthandNotation, ClassLoader classLoader)
     {
-        injectDependency(new Dependency(shorthandNotation), classLoader);
+        DependencyInjector.injectDependency(new Dependency(shorthandNotation), classLoader);
     }
 
     public static void injectDependency(String groupId, String artifactId, String version, ClassLoader classLoader)
     {
-        injectDependency(new Dependency(groupId, artifactId, version), classLoader);
+        DependencyInjector.injectDependency(new Dependency(groupId, artifactId, version), classLoader);
     }
 
     public static void injectDependency(String shorthandNotation, String repository, ClassLoader classLoader)
     {
-        injectDependency(new Dependency(shorthandNotation, repository), classLoader);
+        DependencyInjector.injectDependency(new Dependency(shorthandNotation, repository), classLoader);
     }
 
     public static void injectDependency(String shorthandNotation, Repository repository, ClassLoader classLoader)
     {
-        injectDependency(new Dependency(shorthandNotation, repository), classLoader);
+        DependencyInjector.injectDependency(new Dependency(shorthandNotation, repository), classLoader);
     }
 
     public static void injectDependency(String groupId, String artifactId, String version, String repository, ClassLoader classLoader)
     {
-        injectDependency(new Dependency(groupId, artifactId, version, repository), classLoader);
+        DependencyInjector.injectDependency(new Dependency(groupId, artifactId, version, repository), classLoader);
     }
 
     public static void injectDependency(String groupId, String artifactId, String version, Repository repository, ClassLoader classLoader)
     {
-        injectDependency(new Dependency(groupId, artifactId, version, repository), classLoader);
+        DependencyInjector.injectDependency(new Dependency(groupId, artifactId, version, repository), classLoader);
     }
 
     public static void injectDependency(Dependency dependency, ClassLoader classLoader)
@@ -106,7 +129,7 @@ public final class DependencyInjector
         }
         try
         {
-            ADD_URL_METHOD.invoke(classLoader, dependencyDestination.toURI().toURL());
+            DependencyInjector.ADD_URL_METHOD.invoke(classLoader, dependencyDestination.toURI().toURL());
         }
         catch (Exception exception)
         {
